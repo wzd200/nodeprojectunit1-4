49afd48f4945285a44a2a7152556b3b7bafe6b79
William Dye <williamdye@williams-mbp.myfiosgateway.com>
2021-06-03 17:58:20 -0400

    Added / path

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ if (process.env.NODE_ENV === 'production') {
 }
 const PORT = process.env.PORT || 5000
 
+server.get('/', (req, res) => {
+    res.json({ message: 'Welcome to the Will Server!!' })
+})
+
 server.get('/api', (req, res) => {
   res.json({ message: `${process.env.COHORT} ROCKS` })
 })
